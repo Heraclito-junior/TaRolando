aacae697e8306a75d3d5b633825c4f8d51a266dc
Heraclito-junior <hjuninho94@hotmail.com>
2019-12-05 07:22:20 -0300

    ajeitando coisas na interface

--- src/main/java/framework/br/com/caelum/vraptor/negocio/ConviteNegocio.java
+++ src/main/java/framework/br/com/caelum/vraptor/negocio/ConviteNegocio.java
@@ -5,13 +5,6 @@ import java.util.List;
 public interface ConviteNegocio<T> {
 
 
-    public void aceitar(Long id); 
-    
-
-    public void rejeitar(Long id);
-
-    public void remover(Long id);
-    
     public List<T> meusConvites();
 
     public List<T> meusConvitesEnviados();
